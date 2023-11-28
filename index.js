197e034d949e7c04cce5336984029d8965e26890
Shail Dave <shailjdave72000@gmail.com>
2023-11-28 23:53:47 +0530

    TDD Step 1.

---
+++ index.js
@@ -0,0 +1,44 @@
+const add = (numbers) => {
+
+    try {
+        if (numbers) {
+            if (typeof numbers == 'string') {
+
+                const numArr = numbers.trim().split(',');
+                const numLen = numArr.length;
+
+                if (numLen == 1) {
+                    if (parseFloat(numArr[0]))
+                        return numArr[0];
+                    else
+                        return 'value is not a number.'
+                } else {
+                    const sum = numArr.reduce((sum, num) => {
+                        if (parseFloat(num))
+                            return parseFloat(num) + parseFloat(sum);
+                    })
+                    if (sum)
+                        return sum;
+                    else
+                     return 'something went wrong while performing the Addition.'
+                }
+
+            } else {
+                return 'numbers must be of type string.'
+            }
+        } else {
+            if (typeof numbers == 'string' && numbers.trim().length == 0)
+                return 0;
+            else
+                return 'numbers should not be a falsy value.'
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+// console.info(add("1,2"))
+
+
+module.exports = { add }    
---
+++ test/test.js
@@ -0,0 +1,36 @@
+const { add } = require('..');
+const { expect } = require('chai');
+const { describe } = require('mocha');
+
+
+describe('String Calculator ', function () {
+
+    it('should return message when a falsy value is passed.', function () {
+        expect(add(null)).to.equal('numbers should not be a falsy value.');
+    });
+
+    it('should return message when anything other than the string is passed.', function () {
+        expect(add(-1)).to.equal('numbers must be of type string.');
+    });
+
+    it('should return "0" when the "" is passed.', function () {
+        expect(add("")).to.equal(0);
+    });
+
+    it('should return the passed number itself when only a single number is passed.', function () {
+        expect(add("10")).to.equal("10");
+    });
+
+    it('should return message when instead of a number string is passed.', function () {
+        expect(add("ram")).to.equal("value is not a number.");
+    });
+
+    it('should return the addition of the numbers when two numbers are passed.', function () {
+        expect(add("3,4")).to.equal(7);
+    });
+
+    it('should return the message when appropriate argument is not passed.', function () {
+        expect(add("3,two")).to.equal("something went wrong while performing the Addition.");
+    });
+
+});
